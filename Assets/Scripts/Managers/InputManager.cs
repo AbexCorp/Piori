de1074a2c75415ea56ac6982bfc3c53bfbcc412e
AbexCorp <bartek.fryczek@onet.pl>
2024-08-15 19:53:06 +0200

    Mouse input UI priority

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
     static InputManager Instance;
 
+    //Components
+
+    //Inspector References
     [SerializeField]
     private Camera _camera;
     [SerializeField]
     private LayerMask _mouseInteractable;
+    [SerializeField]
+    private GraphicRaycaster _graphicRaycaster;
 
-    private Vector2 mousePosition;
+    //Code References
 
-    private IMouseInteractions objectUnderCursor;
+    //Fields & Properties
+    private Vector2 _mousePosition;
+    private IMouseInteractions _objectUnderCursor;
+    private PointerEventData _pointerEventData;
+
 
     private void Awake()
     {
@@ -23,6 +34,7 @@ public class InputManager : MonoBehaviour
         if(_camera == null)
             _camera = Camera.main;
         GetComponent<PlayerInput>().camera = _camera;
+        _pointerEventData = new(null);
     }
 
     private void Update()
@@ -32,7 +44,7 @@ public class InputManager : MonoBehaviour
 
     private void HandleMouseHover()
     {
-        Vector2 worldMousePosition = _camera.ScreenToWorldPoint(mousePosition);
+        Vector2 worldMousePosition = _camera.ScreenToWorldPoint(_mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(origin:worldMousePosition, direction:Vector2.zero, distance:1, layerMask:_mouseInteractable); //Change this to raycast all if problems with clicking towers
 
         if (hit)
@@ -40,28 +52,35 @@ public class InputManager : MonoBehaviour
             hit.collider.gameObject.TryGetComponent<IMouseInteractions>(out IMouseInteractions clickable);
             if(clickable != null)
             {
-                if(clickable != objectUnderCursor)
+                if(clickable != _objectUnderCursor)
                 {
-                    objectUnderCursor?.OnMouseHoverLeave();
-                    objectUnderCursor = clickable;
+                    _objectUnderCursor?.OnMouseHoverLeave();
+                    _objectUnderCursor = clickable;
                     clickable?.OnMouseHoverEnter();
                 }
             }
             else
-                objectUnderCursor?.OnMouseHoverLeave();
+                _objectUnderCursor?.OnMouseHoverLeave();
         }
         else
-            objectUnderCursor?.OnMouseHoverLeave();
+            _objectUnderCursor?.OnMouseHoverLeave();
     }
 
 
     public void MousePositionAction(InputAction.CallbackContext context)
     {
-        mousePosition = context.ReadValue<Vector2>();
+        _mousePosition = context.ReadValue<Vector2>();
     }
     public void OnMouseLeftClick(InputAction.CallbackContext context)
     {
-        Vector2 worldMousePosition = _camera.ScreenToWorldPoint(mousePosition);
+        _pointerEventData.position = _mousePosition;
+        List<RaycastResult> results = new();
+        _graphicRaycaster.Raycast(_pointerEventData, results);
+        if (results.Count > 0 || results == null)
+            return;
+
+
+        Vector2 worldMousePosition = _camera.ScreenToWorldPoint(_mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(origin:worldMousePosition, direction:Vector2.zero, distance:1, layerMask:_mouseInteractable); //Change this to raycast all if problems with clicking towers
 
         if (hit)
